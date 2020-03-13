87c01708015200332eb069d2d820b9376889da90
Robinson Marquez <nosnibor1989@gmail.com>
2020-03-12 21:24:27 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Robinson Marquez',
+    author_email='nosnibor1989@gmail.com',
+    description='A utility for backing up postgres databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/nosnibor89/pgbackup",
+    packages=find_packages('src')
+)
